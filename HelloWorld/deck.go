7db7637ed06728444de061f05ec2ec0d9cb39044
Luan Vu <luanvv@vnext.com.vn>
2020-11-25 18:31:07 +0700

    add interface

--- HelloWorld/deck.go
+++ HelloWorld/deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -10,18 +16,48 @@ func (d deck) print() {
 	}
 }
 
-func (d deck) shuffle() {
+func (d deck) shuffle(times int) error {
+	if times > 10 {
+		return fmt.Errorf("You shuffle too many times")
+	}
+	if times < 1 {
+		return nil
+	}
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+	return d.shuffle(times - 1)
 }
 
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Hearts", "Diamnds", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, suit+" of "+value)
 		}
 	}
 	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func readFromFile(filename string) ([]string, error) {
+	bs, error := ioutil.ReadFile(filename)
+	return strings.Split(string(bs), ","), error
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	cards := newDeck()
-	fmt.Println(cards)
-	cards.print()
+	shuffleError := cards.shuffle(11)
+	if shuffleError != nil {
+		fmt.Println(shuffleError)
+		os.Exit(-1)
+	}
+	// hand, remainingCards := deal(cards, 5)
+	const fileName string = "myCards"
+	writeError := cards.saveToFile(fileName)
+	if writeError != nil {
+		fmt.Println("Write file error.", writeError)
+		os.Exit(-1)
+	}
+	cardsFromFile, readError := readFromFile(fileName)
+	if readError != nil {
+		fmt.Println("Read from file error.", readError)
+		os.Exit(-1)
+	}
+	for _, card := range cardsFromFile {
+		fmt.Println(card)
+	}
 }
